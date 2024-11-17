e8a8a4cae7342c99213dd9ad71d12ca016d879f5
silomaben <kaybernard449@gmail.com>
2024-11-17 22:16:27 +0300

    solved the double admin url error

--- Zzenplay/urls.py
+++ Zzenplay/urls.py
@@ -7,7 +7,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('about/', views.about, name="about"),
     path('service/', views.service, name="service"),
